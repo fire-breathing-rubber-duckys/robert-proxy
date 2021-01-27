5daaed0324b4f5923019ea25156aa6ffc67f2d6b
Rob <robertlopezca@gmail.com>
2021-01-26 17:22:16 -0800

    Added logic to handle /product/ request, still only returns JSON

--- reRouter.js
+++ reRouter.js
@@ -4,7 +4,7 @@ const reRouter = (req, res, next) => {
   const servers = ['http://localhost:3001', 'http://localhost:1337', 'http://localhost:3002'];
   const { url } = req;
 
-  const route = (serverLoc, url, type) => {
+  const route = (serverLoc, url, param) => {
     const location = `${serverLoc}${url}`;
     request(location)
       .on('error', (error) => console.log(error))
@@ -17,6 +17,12 @@ const reRouter = (req, res, next) => {
     route(servers[1], url);
   } else if (url.includes('reviews')) {
     route(servers[2], url);
+  } else if (url.includes('/product/')) {
+    let arr = url.split('/');
+    let num = arr[2];
+    request(`${servers[0]}/api/product/${num}`)
+      .on('error', (error) => console.log(error))
+      .pipe(res);
   } else {
     next();
   }
